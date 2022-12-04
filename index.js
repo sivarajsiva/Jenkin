898a19ca3a73aeb3013fc81adb46c8dd571ea0c3
sivarajsiva <sivaraj322@gmail.com>
2022-12-04 18:30:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const port = 3100;
 
-app.get('/', (req, res) => res.send('Welcome!'));
+app.get('/', (req, res) => res.send('Welcome to NodeJs!'));
 app.get('/check', function(request, response){
     response.sendFile(__dirname +'/index.html');
 });
